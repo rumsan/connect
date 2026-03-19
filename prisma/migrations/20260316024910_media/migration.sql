d022aed67d7436fe8668c183636ff38b0fa0514c
bipinparajuli <bipinprjl@gmail.com>
2026-03-19 11:07:58 +0545

    fix:removed variable migration

--- prisma/migrations/20260316024910_media/migration.sql
+++ prisma/migrations/20260316024910_media/migration.sql
@@ -1,9 +1,1 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `variables` on the `tbl_templates` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "tbl_templates" DROP COLUMN "variables",
 ADD COLUMN     "media" TEXT[];
